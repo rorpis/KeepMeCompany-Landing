b58d136b54d34d01ec6c19ffa7d35f51f6bc797a
Rodrigo Orpis <rodrigoorpis@MacBook-Pro-de-Rodrigo.local>
2024-07-20 16:46:32 +0100

    minimal 2 pages example and with docker dev

---
+++ app/page1/page.js
@@ -0,0 +1,10 @@
+import styles from '../(styles)/Page1.module.css';
+
+export default function Page1() {
+  return (
+    <div className={styles.page}>
+      <h1>Page 1</h1>
+      <p>This is Page 1 with its own CSS module.</p>
+    </div>
+  );
+}
