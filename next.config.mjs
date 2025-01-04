9e42720d9314e5dc4390a6bdc82ab3ab5a2bb4d6
Rodrigo Orpis <r.orpis@gmail.com>
2025-01-04 12:24:05 +0100

    locale next config fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,16 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   i18n: {
+    // These are all the locales you want to support in your application
+    locales: ['en-UK', 'es-ES'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'en-UK',
+    // This is a list of locale domains and the default locale they
+    // should handle (these are only required when setting up domain routing)
+    // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
     localeDetection: false, // We'll handle this in middleware
-  }
+  },
 };
 
 export default nextConfig;
