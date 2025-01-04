f9ff50c88887593f50ecc3342cfaa1cfdcd7253c
Rodrigo Orpis <r.orpis@gmail.com>
2025-01-04 12:26:51 +0100

    manual redirections

--- app/page.js
+++ app/page.js
@@ -4,5 +4,13 @@ import { redirect } from 'next/navigation';
 import { i18nConfig } from './config/i18n';
 
 export default function Page() {
-  redirect(`/${i18nConfig.defaultLocale}`);
+  return null; // The middleware will handle the redirect
+}
+
+// Pre-render the default paths
+export function generateStaticParams() {
+  return [
+    { locale: 'en-UK' },
+    { locale: 'es-ES' },
+  ];
 }
--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,11 @@ const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   trailingSlash: true,
-  i18n: {
-    // These are all the locales you want to support in your application
-    locales: ['en-UK', 'es-ES'],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: 'en-UK',
-    // This is a list of locale domains and the default locale they
-    // should handle (these are only required when setting up domain routing)
-    // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
-    localeDetection: false, // We'll handle this in middleware
+  generateStaticParams: async () => {
+    return [
+      { locale: 'en-UK' },
+      { locale: 'es-ES' },
+    ];
   },
 };
 
