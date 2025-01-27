b61fac55986fe97df002af3c16ef5a27285a865c
Rodrigo Orpis <r.orpis@gmail.com>
2025-01-27 14:53:59 +0100

    next inline loading scripts config

--- next.config.mjs
+++ next.config.mjs
@@ -28,7 +28,7 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' https://app.keepmecompanyai.com https://www.youtube.com https://youtu.be",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: https://app.keepmecompanyai.com https://www.youtube.com https://youtu.be",
               "style-src 'self' 'unsafe-inline'",
               "font-src 'self'",
               "img-src 'self' https:",
